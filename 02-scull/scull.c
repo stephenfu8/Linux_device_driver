b8b0fcab88d56fbe9343764d28263cf85e02a370
stephen-wanpeng <stephen-wanpeng.fu@cn.abb.com>
2019-09-15 18:21:52 +0800

    register cdev

--- 02-scull/scull.c
+++ 02-scull/scull.c
@@ -6,7 +6,11 @@
 #include <linux/fs.h>	//for register char dev. 
 
 #include <linux/cdev.h> // for cdev add
-
+#include <linux/slab.h> // for kmalloc
+void clean_module(void);
+/*******************************************************************************
+* module parameter  charp int uint  short ushort array
+*******************************************************************************/
 static int scull_major = 0;
 static int scull_minor = 0;
 static int scull_number= 1;
@@ -23,10 +27,32 @@ module_param(scull_quantum, int,S_IRUGO);
 module_param(message, charp, S_IRUGO);
 module_param_array(mesg_array, int, &a_num, S_IRUGO); //comma
 
+
+/*******************************************************************************
+* char device 
+*******************************************************************************/
+// define a kind of scull device implement 
+struct scull_cdev {
+	int quantum;
+	int qset;
+	unsigned long size;
+	struct cdev cdev;
+};
+struct file_operations scull_fops = {
+	.owner = THIS_MODULE,
+	.open = NULL,
+	.read = NULL,
+	.write = NULL,
+	.release = NULL,
+};
+//define a pubilic  device ID
+static dev_t dev_id;
+//define a structure point to scull char devices.
+static struct scull_cdev * scull_cdevice;
+
 /*******************************************************************************
 * module init function
 *******************************************************************************/
-dev_t dev_num;
 static int __init scull_init(void)
 {
 	int result = 0;
@@ -43,6 +69,8 @@ static int __init scull_init(void)
 			printk(KERN_WARNING "mesg array %d:%d", a_num, mesg_array[a_num]);
 	}
 //3. char device  register
+// get dev number register
+//
 /*** key function***************************************************************
 * 1. old version register char device
 *	register_chrdev     unregister_chrdev
@@ -57,28 +85,75 @@ static int __init scull_init(void)
 *******************************************************************************/
 	{
 		if (scull_major == 0 )	{ // We not get a device major, dynamic regester 
-			result = alloc_chrdev_region(&dev_num, 0, scull_number, "scull_dyn");
+			result = alloc_chrdev_region(&dev_id, 0, scull_number, "scull_dyn");
 		}
 		else {	//manual regester the device
-			dev_num = MKDEV(scull_major, scull_minor);
-			result = register_chrdev_region(dev_num, scull_number, "scull_sta");
+			dev_id = MKDEV(scull_major, scull_minor);
+			result = register_chrdev_region(dev_id, scull_number, "scull_sta");
 		}
 		if ( result < 0 ) {
 			printk(KERN_WARNING "scull: can not get a major");
 		}else{
-			printk(KERN_WARNING "Major:%d Minor:%d", MAJOR(dev_num), MINOR(dev_num));
+			scull_major = MAJOR(dev_id);
+			printk(KERN_WARNING "Major:%d Minor:%d", scull_major, MINOR(dev_id));
+		}
+	}
+	{	// allocate  memery for scull cher device.
+		int i;
+		scull_cdevice = kmalloc( sizeof(struct scull_cdev) * scull_number, GFP_KERNEL);
+		if ( scull_cdevice == NULL ) {
+			printk(KERN_ERR "kmalloc: no memery for scull char device");
+			goto failed_exit;
+		}
+		// initilize the allocate memery
+		memset(scull_cdevice, 0 ,  sizeof(struct scull_cdev) * scull_number);
+		// config and initilize char device 
+		for (i = 0; i< scull_number; i++) {
+		//config variables
+			scull_cdevice[i].quantum = scull_quantum;
+			scull_cdevice[i].qset = scull_qset;
+			scull_cdevice[i].size = 0;
+		//initilize the cdev structure
+			cdev_init( &scull_cdevice[i].cdev, &scull_fops);
+			scull_cdevice[i].cdev.owner = THIS_MODULE;
+			scull_cdevice[i].cdev.ops = &scull_fops;
+		//add char device.
+			dev_id = MKDEV(scull_major, scull_minor + i);
+			result = cdev_add( &scull_cdevice[i].cdev, dev_id, 1);
+			if (result  < 0) {
+				printk(KERN_ERR "add cdev device failed");
+				break;
+			} else {
+				printk(KERN_NOTICE "cdev major:%d minor:%d", MAJOR(dev_id), MINOR(dev_id));
+			}
 		}
 	}
 	return result;
+failed_exit:	// how to deal with errors
+	// clean up 
+	clean_module();
+	return result;
+}
+void clean_module(void)
+{
+	printk(KERN_WARNING "Clean up Module init");
+
+// Last-2. free malloc memery
+	kfree(scull_cdevice);
+// Last-1. unregister cdev
+	unregister_chrdev_region(dev_id, scull_number);
 }
 /*******************************************************************************
 * module exit function
 *******************************************************************************/
 static void __exit scull_exit(void)
 {
-//1. unregister cdev
-	unregister_chrdev_region(dev_num, scull_number);
-
+	int i;
+//  delete scull char devices cdev_del
+	for ( i = 0; i < scull_number; i++) {
+		cdev_del(&scull_cdevice[i].cdev);	
+	}
+	clean_module();
 	printk("Goodbye kernel\n");
 }
 
